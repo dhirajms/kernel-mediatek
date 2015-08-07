5f3e60332bfba543a3e76598a36581e9454769a6
Kapileshwar Singh <kapileshwar.singh@arm.com>
2015-08-07 17:28:00 +0100

    [PATCH] FIXUP: Fix incorrect default config param for CPU_FREQ_GOV_SCHED
    This problem fixes build failure when CONFIG_CPU_FREQ_DEFAULT_GOV_SCHED
    is set.
    
    Change-Id: I9df0078c308794f82acdf767cfcdddafdc1a640e
    Signed-off-by: Kapileshwar Singh <kapileshwar.singh@arm.com>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -495,8 +495,8 @@ extern struct cpufreq_governor cpufreq_gov_conservative;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTERACTIVE)
 extern struct cpufreq_governor cpufreq_gov_interactive;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_interactive)
-#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SCHED_GOV)
-extern struct cpufreq_governor cpufreq_gov_sched_gov;
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SCHED)
+extern struct cpufreq_governor cpufreq_gov_sched;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_sched)
 #endif
 
