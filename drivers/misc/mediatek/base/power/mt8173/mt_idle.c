287542cbcbf4a6ddff971c0b3ec7e11dd30ec1e4
Anmin Hsu <anmin.hsu@mediatek.com>
2015-09-08 03:35:12 +0800

    [ALPS02166413] idle: mediatek: Enable deep idle and slow idle
    [Detail]
    Enable deep idle and slow idle by default on MT8173.
    
    [Solution]
    Enable deep idle and slow idle by default.
    
    [Feature] Power Management
    
    MTK-Commit-Id: 6d98b426491807f4dc1fd27e85da0f1bdff4f4a4
    
    Change-Id: I1b80583947142f2312aa47fdb6dedc9cb3b37ebe
    Signed-off-by: James Liao <jamesjj.liao@mediatek.com>
    CR-Id: ALPS02166413

--- drivers/misc/mediatek/base/power/mt8173/mt_idle.c
+++ drivers/misc/mediatek/base/power/mt8173/mt_idle.c
@@ -98,10 +98,10 @@ enum {
 
 /* Idle handler on/off */
 static int idle_switch[NR_TYPES] = {
-	0,			/* dpidle switch */
+	1,			/* dpidle switch */
 	0,			/* soidle switch */
 	0,			/* mcidle switch */
-	0,			/* slidle switch */
+	1,			/* slidle switch */
 	1,			/* rgidle switch */
 };
 
