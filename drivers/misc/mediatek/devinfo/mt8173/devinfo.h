6b891e6246701fcc641e78891e49a01bb7497b4a
Greg Hackmann <ghackmann@google.com>
2015-11-10 16:37:56 -0800

    drivers: mediatek: devinfo: remove userspace-facing API
    devinfo has two userspace-facing APIs which expose the contents of a
    specific Device-Tree node.  This information is already accessible
    through /proc/device-tree.
    
    Change-Id: Id8092831d185cc321d6db17da6c3c3fbbd84202d
    Signed-off-by: Greg Hackmann <ghackmann@google.com>

--- drivers/misc/mediatek/devinfo/mt8173/devinfo.h
+++ drivers/misc/mediatek/devinfo/mt8173/devinfo.h
@@ -5,14 +5,6 @@
  * MODULE DEFINITION
  *****************************************************************************/
 #define MODULE_NAME	 "[devinfo]"
-#define DEV_NAME		"devmap"
-#define MAJOR_DEV_NUM	196
- /*****************************************************************************
- * IOCTL DEFINITION
- *****************************************************************************/
-#define DEV_IOC_MAGIC	   'd'
-#define READ_DEV_DATA	   _IOR(DEV_IOC_MAGIC,  1, unsigned int)
-#define DEV_IOC_MAXNR	   (10)
 
 #ifdef CONFIG_OF
 /*device information data*/
